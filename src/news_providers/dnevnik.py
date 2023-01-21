136744086069ccdd1fbf4c427be18be1d5a3b266
Alek Boninski <aboninski@gmail.com>
2023-01-21 17:20:02 +0200

    Fixes

--- src/news_providers/dnevnik.py
+++ src/news_providers/dnevnik.py
@@ -9,22 +9,15 @@ class DnevnikCrawler:
     article_title_selector = '.main-content > .content > h1'
     article_content_selector = '.article-content > p'
 
-    def __init__(self, articles_to_fetch):
-        self.articles_to_fetch = articles_to_fetch
-
-    def __fetch_latest_article_links_and_titles(self):
+    def __fetch_latest_article_links_and_titles(self, articles_to_fetch=1):
         latest_news_html = requests.get(self.latest_news_url).text
         soup = bs4.BeautifulSoup(latest_news_html)
         article_links = [('{base_url}{path}'.format(base_url=self.base_url, path=element.get('href')), element.text) for element in soup.select(self.article_link_selector)]
-        return article_links[0:self.articles_to_fetch]
+        return article_links[0:articles_to_fetch]
 
     def __fetch_article(self, link, fallback_title=''):
-        # article_link = 'https://www.dnevnik.bg/bulgaria/2023/01/21/4440961_rumen_radev_oshte_sum_na_poziciiata_che_bulgariia_ne/'
-        # article_link = 'https://www.dnevnik.bg/bulgaria/2023/01/21/4440940_geshev_razsledvaniiata_za_kushtata_v_barselona_i/'
-        article_link = 'https://www.dnevnik.bg/evropa/2023/01/21/4435082_samo_v_edna_strana_v_evropa_horata_sa_nedovolni_ot/'
         try:
-            # article_html = requests.get(link).text
-            article_html = requests.get(article_link).text
+            article_html = requests.get(link).text
             soup = bs4.BeautifulSoup(article_html)
             title_element = soup.select(self.article_title_selector)
             article_content_elements = list(filter(lambda element: bool(element.text.strip()) and element.next.name != 'figure', soup.select(self.article_content_selector)))
@@ -40,12 +33,15 @@ class DnevnikCrawler:
             pass
 
 
-    def fetch_articles(self):
-        article_links_and_titles = self.__fetch_latest_article_links_and_titles()
+    def fetch_articles(self, articles_to_fetch=1):
+        article_links_and_titles = self.__fetch_latest_article_links_and_titles(articles_to_fetch)
         articles = [self.__fetch_article(link, title) for (link, title) in article_links_and_titles]
         return articles
         
 
     def test(self):
-        article = self.__fetch_article('')
+        # article_link = 'https://www.dnevnik.bg/bulgaria/2023/01/21/4440961_rumen_radev_oshte_sum_na_poziciiata_che_bulgariia_ne/'
+        # article_link = 'https://www.dnevnik.bg/bulgaria/2023/01/21/4440940_geshev_razsledvaniiata_za_kushtata_v_barselona_i/'
+        article_link = 'https://www.dnevnik.bg/evropa/2023/01/21/4435082_samo_v_edna_strana_v_evropa_horata_sa_nedovolni_ot/'
+        article = self.__fetch_article(article_link)
         print(article)
